95c99e8e23bc99475e2c65706ec63a593a7b988f
Alvin Philips <alvinphilips257@gmail.com>
2021-07-29 06:28:23 +0530

    Removed need for compiler flags, and ensured msvc compatibility

--- bigman.cpp
+++ bigman.cpp
@@ -2,15 +2,20 @@
 #include <string.h>
 
 int main() {
-    char name[8]; // Our string can hold eight bytes
-    int really_important_number;
+    struct User {
+        char name[8]; // Our string can hold eight bytes
+        int really_important_number;
+    };
 
-    really_important_number = 420; // Hehe
-    std::cout << really_important_number << std::endl;
-    strcpy(name, "Leonardino"); // Set our name
-    
-    std::cout << "My name is " << name << " and my password is " << really_important_number << "\n";
+    User user;
+
+    user.really_important_number = 420; // Hehe
+    std::cout << user.really_important_number << std::endl;
+    strcpy(user.name, "Leonardino"); // Set our name
+    std::cout << "My name is " << user.name << " and my password is " << user.really_important_number << "\n";
     // Uh oh something happened to our password
+    
+    std::cout << &user.name << ", " << &user.really_important_number << std::endl;
     
     return 0;
 }
